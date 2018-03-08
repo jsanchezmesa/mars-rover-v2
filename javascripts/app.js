4c0c61adaea7cccee986de7ec833109e56e28d15
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-03-08 16:42:34 +0100

    Unnecessary comments removed

--- javascripts/app.js
+++ javascripts/app.js
@@ -34,19 +34,15 @@ function turnLeft(rover){
   switch( rover.direction ) {
     case "N":
       rover.direction = "W";
-      //console.log("Rover has turned to west");
       break;
     case "E":
       rover.direction = "N";
-      //console.log("Rover has turned to north");
       break;
     case "S":
       rover.direction = "E";
-      //console.log("Rover has turned to east");
       break;
     case "W":
       rover.direction = "S";
-      //console.log("Rover has turned to south");
       break;
   }
 }
@@ -55,19 +51,15 @@ function turnRight(rover){
   switch( rover.direction ) {
     case "N":
       rover.direction = "E";
-      //console.log("Rover has turned to east");
       break;
     case "E":
       rover.direction = "S";
-      //console.log("Rover has turned to south");
       break;
     case "S":
       rover.direction = "W";
-      //console.log("Rover has turned to west");
       break;
     case "W":
       rover.direction = "N";
-      //console.log("Rover has turned to north");
       break;
   }
 }
@@ -161,7 +153,6 @@ function moveForward(rover, grid){
       break;
   }
   
-  //console.log( "Rover moved to (" + rover.x + ", " + rover.y + ")" );
   // add movement to travel log if rover has moved
   if( movement ) {
     rover.travelLog.push( [rover.x, rover.y] );
